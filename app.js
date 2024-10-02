a92693c63a88ed5924ca53b6f07425d56fd90505
Jaxon <xudoyberdiyevjahon8@gmail.com>
2024-10-02 23:28:18 +0500

    add middleware

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const fileUpload = require('express-fileupload')
+const requestTime = require('./middlewares/request-time')
+
 
 const app = express()
+
 
 app.use(express.json())
 app.use(express.static('static'))
 app.use(fileUpload({}))
+app.use(requestTime)
 
 // Routes
 app.use('/api/post', require('./routers/post-route'))
