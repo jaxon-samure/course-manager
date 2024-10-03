868859ed3601207ffd0a61c2751327cfacff3aed
Jaxon <xudoyberdiyevjahon8@gmail.com>
2024-10-03 20:39:07 +0500

    /

--- models/post-model.js
+++ models/post-model.js
@@ -4,6 +4,7 @@ const postSchema = new Schema({
 	title: { type: String, required: true },
 	body: { type: String, required: true },
 	picture: { type: String },
-})
+	})
 
 module.exports = model('Post', postSchema)
+
